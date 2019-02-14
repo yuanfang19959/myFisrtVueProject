25246d64f51719ebc8da8aa91daa84a84b3cf966
zheng <544080261@qq.com>
2019-02-14 21:01:10 +0800

    解决了组件滑动的问题

--- src/components/beautiful/beau.vue
+++ src/components/beautiful/beau.vue
@@ -33,12 +33,23 @@
 </template>
 
 <script>
-// 	import mui from '../../lib/mui/js/mui.js'
-// 	
-// 	mui('.mui-scroll-wrapper').scroll({
-// 	deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
-// });
+	import mui from '../../lib/mui/js/mui.js'
+	
+	
+
+export default{
+	mounted  (){
+		mui('.mui-scroll-wrapper').scroll({
+	deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+})
+	}
+}
 </script>
 
 <style>
+*{
+	touch-action: pan-y;
+}
 </style>
+
+
